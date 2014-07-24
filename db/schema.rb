dc21b180deda99498def343513c584efcb6d3be1
Colin Percy <colin.h.percy@outlook.com>
2014-07-23 20:28:02 -0700

    updated style of devise user pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140723043042) do
+ActiveRecord::Schema.define(version: 20140724022442) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
