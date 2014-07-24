3be8394801a7cf254c8412ef3a36a4eac7d8b1c5
Colin Percy <colin.h.percy@outlook.com>
2014-07-23 21:43:02 -0700

    fixed pins controller error copied from omr site

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,48 +1,40 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /pins
   def index
     @pins = Pin.all
   end
 
-  # GET /pins/1
   def show
   end
 
-  # GET /pins/new
   def new
     @pin = Pin.new
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
   def create
     @pin = Pin.new(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render :new
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /pins/1
   def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.'
-      else
-        render :edit
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
-  # DELETE /pins/1
   def destroy
     @pin.destroy
-      redirect_to pins_url, notice: 'Pin was successfully destroyed.'
-    end
+    redirect_to pins_url
   end
 
   private
