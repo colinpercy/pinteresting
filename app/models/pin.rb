815c4feb73fe159ba04da7b2ac81e1f322b552da
Colin Percy <colin.h.percy@outlook.com>
2014-07-23 22:49:25 -0700

    troubleshooting failed migration of association by undoing changes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,2 @@
 class Pin < ActiveRecord::Base
-
-belongs_to :user
-
 end
--- db/migrate/20140724053437_add_user_id_to_pins.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToPins < ActiveRecord::Migration
-  def change
-    add_column :pins, :user, :string
-    add_column :pins, :id, :integer
-    add_index :pins, :id
-  end
-end
